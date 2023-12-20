5f3aca7a5b2562a37bb14780b216dea4142eff95
marzhanm <marzanmutalova@gmail.com>
2023-12-20 15:28:36 +0600

    Changes by ajda

--- src/main/java/DatabaseConnector.java
+++ src/main/java/DatabaseConnector.java
@@ -1,9 +1,14 @@
 package main.java;
 import java.sql.*;
 import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 
-class DatabaseConnector {
+
+
+public class DatabaseConnector {
     private static final String URL = "jdbc:postgresql://localhost:5431/5Assignment";
     private static final String USER = "postgres";
     private static final String PASSWORD = "12345";
